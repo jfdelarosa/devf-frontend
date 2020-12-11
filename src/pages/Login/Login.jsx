0c7ece295878c6be5ad86200d26e3c8f57d761fc
Juan Fernando <jfdelarosa@outlook.com>
2020-12-11 17:48:42 -0600

    Se crea el login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,69 @@
-import React from 'react'
-import {
-  
-} from '../../components/index.js'
-
+import React, { Fragment, useContext } from "react";
+import { Redirect } from "react-router-dom";
+import { Card, Form, Input, Button } from "antd";
+import { auth } from "../../services";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
-  return (
-    <React.Fragment>
-    </React.Fragment>
-  )
-}
+  const { logIn, isAuth } = useContext(AuthContext);
 
-export default Login
+  const onFinish = async (obj) => {
+    const { data } = await auth.login(obj);
+    logIn(data.token);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Fragment>
+      <Card style={{ width: 300, margin: "0 auto" }}>
+        <Form
+          layout="vertical"
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Ingresa tu email!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Contraseña"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Ingresa tu contraseña!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+      {isAuth && <Redirect to="/" />}
+    </Fragment>
+  );
+};
+
+export default Login;
--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,12 +1,13 @@
-  
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://instal-clone.herokuapp.com/api/v1/'
-  baseURL: 'http://localhost:3006/api/v1/'
-})
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3006/api/v1/"
+      : "https://instal-clone.herokuapp.com/api/v1/",
+});
 
 export const auth = {
-  signup: (body) => api.post('/signup', body),
-  login: (body) => api.post('/login', body),
-}
+  signup: (body) => api.post("/auth/signup", body),
+  login: async (body) => api.post("/auth/login", body),
+};
