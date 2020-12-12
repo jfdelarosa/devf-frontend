26e663033a390f5bf3f19e83545e30e87962237f
Juan Fernando <jfdelarosa@outlook.com>
2020-12-11 19:02:42 -0600

    Rediseño del home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Card, Empty, Button, Row, Col, Badge } from "antd";
 import {
-  NavBar,
-  CommentCard
-} from '../../components/index.js'
-import { Container } from 'semantic-ui-react'
+  CommentOutlined,
+  LikeOutlined,
+  DislikeOutlined,
+} from "@ant-design/icons";
+import { company } from "../../services";
 
+const { Meta } = Card;
+
+const Company = (props) => {
+  return (
+    <Card
+      style={{ width: 200 }}
+      actions={[
+        <LikeOutlined key="like" />,
+        <Badge size="small" count={props.reviews.length}>
+          <CommentOutlined key="comments" />
+        </Badge>,
+        <DislikeOutlined key="dislike" />,
+      ]}
+    >
+      <Meta title={props.name} description={props.location} />
+      <div style={{ marginTop: "1rem" }}>{props.description}</div>
+    </Card>
+  );
+};
+
+const NoCompanies = () => {
+  return (
+    <Empty
+      style={{ margin: 0 }}
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+      description={<span>Aún no hay reviews</span>}
+    >
+      <Button type="primary">Crear una</Button>
+    </Empty>
+  );
+};
 
 const Home = () => {
-  return (
-    <React.Fragment>
-      <NavBar/>
-      <Container>
-        <CommentCard/>
-        <CommentCard/>
-        <CommentCard/>
-      </Container>
-    </React.Fragment>
-  )
-}
+  const [items, setItems] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await company.list();
+      console.log(data.data);
+      setItems(data.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {items.length ? (
+        <Row gutter={16}>
+          {items.map((item) => (
+            <Col>
+              <Company {...item}></Company>
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        <NoCompanies></NoCompanies>
+      )}
+    </>
+  );
+};
+
+export default Home;
